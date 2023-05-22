5c65451bdcc610ac5f1a6f30c504b1cfc54b5662
pairnon <fhollis983@gmail.com>
2023-05-22 09:04:04 -0700

    increased timeout

--- ws.py
+++ ws.py
@@ -12,7 +12,10 @@ server_ip = IP
 rcon_password = RCON_PASS
 rcon_port = int(RCON_PORT)
 
-rcon = mcrcon.MCRcon(server_ip, rcon_password, rcon_port)
+tls_mode = 0
+timeout = 10
+
+rcon = mcrcon.MCRcon(server_ip, rcon_password, rcon_port, tls_mode, timeout)
 
 rcon.connect()
 
