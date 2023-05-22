4034107fa4f65a126d33579f75f7ac0d86eed4e6
pairnon <fhollis983@gmail.com>
2023-05-22 08:49:44 -0700

    update

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-from dotenv import load_dotenv
-import os
+from ws import *
 
-import mcrcon
+exec("/world world")
+exec("/pos1 0,0,0")
+exec("/pos2 100,100,100")
+exec("/set air")
 
-load_dotenv()
-
-IP = os.getenv("IP")
-RCON_PASS = os.getenv("RCON_PASS")
-RCON_PORT = os.getenv("RCON_PORT")
-
-server_ip = IP
-rcon_password = RCON_PASS
-rcon_port = int(RCON_PORT)
-
-rcon = mcrcon.MCRcon(server_ip, rcon_password, rcon_port)
-
-rcon.connect()
-
-command = "time set day"
-response = rcon.command(command)
-print(response) # Print the server's response
-
-rcon.disconnect()
+exit()
