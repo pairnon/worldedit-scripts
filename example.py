afbe2bb33ee4de36ad49e320713c0e4715820e35
pairnon <fhollis983@gmail.com>
2023-05-22 20:32:02 -0700

    updated for OOP

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from ws import *
 
-exec("//world world")
-exec("//pos1 0,0,0")
-exec("//pos2 100,100,100")
-# exec("//set tinted_glass")
-# exec("//set air")
+ws = Ws()
 
-exit()
+ws.exec("//world world")
+ws.exec("//pos1 0,0,0")
+ws.exec("//pos2 100,100,100")
+# ws.exec("//set tinted_glass")
+# ws.exec("//set air")
+
+ws.exit()
