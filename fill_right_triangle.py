eecbfa4b5d71cdb7e8a66e0440fa54ae44a279d4
pairnon <fhollis983@gmail.com>
2023-05-23 14:37:00 -0700

    created fill file

---
+++ fill_right_triangle.py
@@ -0,0 +1,19 @@
+from ws import *
+    
+     # start_pos
+    ##
+   # #
+  #  #
+ #   #
+#    #
+######
+
+server = Server()
+
+c = Command()
+
+start_pos = Pos(-317, -35, 588)
+
+# while(True):
+#     if()
+
